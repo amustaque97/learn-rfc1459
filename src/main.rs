b4042d9c3e972dfef0f7feed11ab0b52bc96649d
Mustaque Ahmed <amustaque97@gmail.com>
2023-03-25 23:56:08 +0530

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,90 @@
+use std::env;
+use std::error::Error;
+
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+
+mod server;
+use server::{Errors, Server};
+
+pub async fn parse_command(buf: &[u8]) -> Vec<String> {
+    let c = String::from_utf8_lossy(&buf);
+    c.strip_prefix("/")
+        .unwrap()
+        .split_whitespace()
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
+
+    let listener = TcpListener::bind(&addr).await?;
+    println!("Server running on {}", addr);
+
+    let mut server = Server::new();
+    server.set_motd("Welcome to the default server\r\n");
+
+    loop {
+        // Async wait for an inbound socket.
+        let (mut socket, addr) = listener.accept().await?;
+        let mut server = server.clone();
+
+        tokio::spawn(async move {
+            // section 8.2 command parsing
+            let mut buf = vec![0; 512];
+
+            println!("Connection request from {}", addr);
+
+            // send message of the day if present
+            match server.motd {
+                Some(msg) => socket
+                    .write_all(msg.as_bytes())
+                    .await
+                    .expect("failed to send message of the day to the client"),
+                None => println!("No message of the day is configured on the server"),
+            }
+
+            // In a loop, read data from the socket and write the data back
+            loop {
+                let n = socket
+                    .read(&mut buf)
+                    .await
+                    .expect("failed to read data from socket");
+
+                if n == 0 {
+                    println!("No data send to the server");
+                    return;
+                } else {
+                    println!(
+                        "Request buffer {:?}",
+                        String::from_utf8_lossy(&buf[0..n - 1])
+                    );
+                }
+
+                let command_list = parse_command(&buf[0..n]).await;
+                let command = command_list[0].as_str();
+                let mut response: (Option<Errors>, String) =
+                    (Some(Errors::UnknownCommand), "Unknown command".to_string());
+
+                match command {
+                    "NICK" => {
+                        response = server.nick_command(command_list[1..].into(), addr).await;
+                    }
+                    "USER" => {
+                        response = server.user_command(command_list[1..].into(), addr).await;
+                    }
+                    _ => todo!("Unknown command"),
+                }
+
+                socket
+                    .write(response.1.as_bytes())
+                    .await
+                    .expect("failed to send response");
+            }
+        });
+    }
+}
---
+++ src/server.rs
@@ -0,0 +1,86 @@
+use std::collections::HashMap;
+use std::net::SocketAddr;
+use std::sync::{Arc, Mutex};
+
+// key: addr
+// Val: nickname, username
+type UserList = Arc<Mutex<HashMap<String, Vec<String>>>>;
+
+pub enum Errors {
+    ErrNoNickNameGiven = 431,
+    ErrNickNameInUse = 433,
+    UnknownCommand = -1,
+}
+
+#[derive(Debug, Clone)]
+pub struct Server {
+    pub motd: Option<&'static str>,
+
+    pub users: UserList,
+}
+
+impl Server {
+    pub fn new() -> Self {
+        Server {
+            motd: None,
+            users: Arc::new(Mutex::new(HashMap::new())),
+        }
+    }
+
+    pub fn set_motd(&mut self, msg: &'static str) {
+        self.motd = Some(msg);
+    }
+
+    pub async fn nick_command(
+        &mut self,
+        command_list: Vec<String>,
+        addr: SocketAddr,
+    ) -> (Option<Errors>, String) {
+        let empty = "".to_string();
+        let nickname = command_list
+            .get(0)
+            .unwrap_or_else(|| &empty)
+            .to_string()
+            .clone()
+            .to_lowercase();
+        println!("Requested nickname {:?}", nickname);
+
+        if nickname.len() < 1 {
+            return (
+                Some(Errors::ErrNoNickNameGiven),
+                "No Nickname given\r\n".to_string(),
+            );
+        }
+
+        let mut user_list = self.users.lock().unwrap();
+        if user_list.contains_key(&nickname) {
+            // todo kill command and kick out all the existing nickname
+            return (
+                Some(Errors::ErrNickNameInUse),
+                format!("{} Nickname is already in use\r\n", nickname),
+            );
+        } else {
+            let mut val = Vec::new();
+            val.push(nickname.clone());
+            user_list.insert(addr.to_string(), val);
+            return (None, format!("Introducing new nick \"{}\" \r\n", nickname));
+        }
+    }
+
+    // todo: implemnt hostname servername and realname command
+    pub async fn user_command(
+        &mut self,
+        command_list: Vec<String>,
+        addr: SocketAddr,
+    ) -> (Option<Errors>, String) {
+        let username = command_list.get(0).unwrap().clone().to_lowercase();
+        println!("Requested username {:?}", username);
+        let mut user_list = self.users.lock().unwrap();
+
+        let val: &mut Vec<String> = user_list.get_mut(&addr.to_string()).unwrap();
+        val.push(username);
+        return (None, "Username registered successfully".to_string());
+    }
+}
+
+// impl Copy for Server {}
